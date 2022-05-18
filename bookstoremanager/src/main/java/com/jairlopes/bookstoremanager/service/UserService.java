949f8d460254810ffd24a9e8178bcb7205616433
unknown <jair.lopes@fatec.sp.gov.br>
2022-05-17 22:02:00 -0300

    Criação de novos usuários parte 1: Camada Service e tests unitários

--- bookstoremanager/src/main/java/com/jairlopes/bookstoremanager/service/UserService.java
+++ bookstoremanager/src/main/java/com/jairlopes/bookstoremanager/service/UserService.java
@@ -1,9 +1,15 @@
 package com.jairlopes.bookstoremanager.service;
 
+import com.jairlopes.bookstoremanager.dto.MessageDTO;
+import com.jairlopes.bookstoremanager.dto.UserDTO;
+import com.jairlopes.bookstoremanager.entity.User;
+import com.jairlopes.bookstoremanager.exception.UserAlreadyExistsException;
 import com.jairlopes.bookstoremanager.mapper.UserMapper;
 import com.jairlopes.bookstoremanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +21,24 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public MessageDTO create(UserDTO userToCreateDTO) {
+        String email = userToCreateDTO.getEmail();
+        String username = userToCreateDTO.getUsername();
+        userRepository.findByEmailOrUsername(email, username)
+                .ifPresent(user -> {
+                    throw new UserAlreadyExistsException(email, username);
+                });
+
+        User userToCreate = userMapper.toModel(userToCreateDTO);
+        User createdUser = userRepository.save(userToCreate);
+
+        String createdUsername = createdUser.getUsername();
+        Long createdUserId = createdUser.getId();
+        String createdUserMessage = String.format("User %s with ID %s successfully created", createdUsername, createdUserId);
+        return MessageDTO.builder()
+                .message(createdUserMessage)
+                .build();
     }
 }
--- bookstoremanager/src/test/java/com/jairlopes/bookstoremanager/service/UserServiceTest.java
+++ bookstoremanager/src/test/java/com/jairlopes/bookstoremanager/service/UserServiceTest.java
@@ -1,13 +1,27 @@
 package com.jairlopes.bookstoremanager.service;
 
 import com.jairlopes.bookstoremanager.builder.UserDTOBuilder;
+import com.jairlopes.bookstoremanager.dto.MessageDTO;
+import com.jairlopes.bookstoremanager.dto.UserDTO;
+import com.jairlopes.bookstoremanager.entity.User;
+import com.jairlopes.bookstoremanager.exception.UserAlreadyExistsException;
 import com.jairlopes.bookstoremanager.mapper.UserMapper;
 import com.jairlopes.bookstoremanager.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -26,4 +40,35 @@ public class UserServiceTest {
     void setup() {
         userDTOBuilder = UserDTOBuilder.builder().build();
     }
+
+    @Test
+    void whenNewUserIsInformedThenItShouldBeCreated() {
+        UserDTO expectedCreatedUserDTO = userDTOBuilder.buildUserDTO();
+        User expectedCreatedUser = userMapper.toModel(expectedCreatedUserDTO);
+        String expectedCreationMessage = "User jairlopes with ID 1 successfully created";
+
+        String email = expectedCreatedUserDTO.getEmail();
+        String username = expectedCreatedUserDTO.getUsername();
+        when(userRepository.findByEmailOrUsername(email, username))
+                .thenReturn(Optional.empty());
+        when(userRepository.save(expectedCreatedUser)).thenReturn(expectedCreatedUser);
+
+        MessageDTO creationMessage = userService.create(expectedCreatedUserDTO);
+
+        assertThat(expectedCreationMessage, is(equalTo(creationMessage.getMessage())));
+    }
+
+    @Test
+    void whenExistingUserIsInformedThenAnExceptionShouldBeThrown() {
+        UserDTO expectedDuplicatedUserDTO = userDTOBuilder.buildUserDTO();
+        User expectedDuplicatedUser = userMapper.toModel(expectedDuplicatedUserDTO);
+
+        String email = expectedDuplicatedUserDTO.getEmail();
+        String username = expectedDuplicatedUserDTO.getUsername();
+        when(userRepository.findByEmailOrUsername(email, username))
+                .thenReturn(Optional.of(expectedDuplicatedUser));
+
+        assertThrows(UserAlreadyExistsException.class, () -> userService.create(expectedDuplicatedUserDTO));
+    }
+
 }
